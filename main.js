2ee0a1651d9b3f5abdeaffc8fdea8737b017ceb4
AlanaKotarski <105052030+AlanaKotarski@users.noreply.github.com>
2024-07-26 10:42:02 -0300

    xx

---
+++ main.js
@@ -0,0 +1,17 @@
+const perguntas = [
+    pergunta1{
+        enunciado: "Você gosta de bolo?",
+        alternativas: [
+            {
+                texto: "Alternativa A",
+                afirmacao: "Você é uma pessoa normal"
+            },
+            {
+                texto: "Alternativa B",
+                afirmação: "Você é chato hein bot"
+            }]
+    },
+    pergunta2{
+
+    }
+]
