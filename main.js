bb38403a8b4de36d5b707de4ccac4a27467ba8ba
AlanaKotarski <105052030+AlanaKotarski@users.noreply.github.com>
2024-08-30 09:29:35 -0300

    f

--- main.js
+++ main.js
@@ -5,33 +5,73 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 
-
 const perguntas = [
-    pergunta1{
-        enunciado: "Você gosta de bolo?",
+    {
+        enunciado: "Pergunta 1",
         alternativas: [
             {
                 texto: "Alternativa A",
-                afirmacao: "Você é uma pessoa normal"
+                afirmacao: "afirmacao da alternativa A."
             },
             {
                 texto: "Alternativa B",
-                afirmação: "Você é chato hein bot"
-            }]
+                afirmacao: "afirmacao da alternativa B."
+            }
+        ]
     },
-    pergunta2{
-
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto: "Alternativa A",
+                afirmacao: "afirmacao da alternativa A."
+            },
+            {
+                texto: "Alternativa B",
+                afirmacao: "afirmacao da alternativa B."
+            }
+        ]
     }
-]
+];
 
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta(){
-    if (atual >= perguntas.lenght) {
-        mostraResultado();
-        return; 
+    if (atual >= perguntas.length){
+        mostraResultado();t
+        return;
     }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",
+            ()=>respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual ++;
+    mostraPergunta();
+}
+
+
+
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "De acordo com suas respostas...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
